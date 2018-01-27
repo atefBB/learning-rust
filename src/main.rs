e149f1425542d48ed316f6c1542abce6c3585bb4
Atef Ben Ali <atef.bettaib@gmail.com>
2018-01-27 07:46:45 +0100

    encapsulate the game logic in a `loop` statement

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,24 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
     println!("The secret number is: {}", secret_number);
+    
+    loop {
+        println!("Please input your guess!");
 
-    println!("Please input your guess!");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
 
-    io::stdin().read_line(&mut guess)
-       .expect("Failed to read line");
+        let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
 
-    let guess: u32 = guess.trim().parse()
-       .expect("Please type a number!");   
+        println!("Your guess: {}", guess);
 
-    println!("Your guess: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("Too win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("Too win!"),
+        }
     }
 }
