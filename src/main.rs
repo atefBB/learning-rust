d2b8b372ad7059a763194fb0c4409c0223ad14fa
Atef Ben Ali <atef.bettaib@gmail.com>
2018-01-27 07:49:24 +0100

    add `break` statement (to allow the user to quit the game if the `guess` match the `secret number`)

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
     println!("The secret number is: {}", secret_number);
-    
+
     loop {
         println!("Please input your guess!");
 
@@ -26,7 +26,10 @@ fn main() {
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("Too win!"),
+            Ordering::Equal => {
+                println!("You win!")
+                break;
+            },
         }
     }
 }
